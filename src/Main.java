ab0243e18d6796e05e5e416bd8f1fffba8b65402
Kris <kristina_lacrima@mail.ru>
2023-07-02 23:05:17 +0300

    reformatecode

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 
 public class Main {
     public static void main(String[] args) {
-       BmiService service = new BmiService();
-       double height = 1.8;
-       int weight = 64;
+        BmiService service = new BmiService();
+        double height = 1.8;
+        int weight = 64;
 
-       double bmi = service.calculate((double) height, weight);
-            System.out.println((int) bmi);
-        }
+        double bmi = service.calculate((double) height, weight);
+        System.out.println((int) bmi);
     }
+}
